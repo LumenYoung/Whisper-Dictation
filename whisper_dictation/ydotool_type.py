4dcc018a256c86248e691354990445a0ddaa3cb7
Tangdi <45258158+TangdizhiH@users.noreply.github.com>
2023-04-11 11:48:48 +0200

    delete unused files

--- whisper_dictation/ydotool_type.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-
-# function type: run ydotool to type the given content
-def type_content(content):
-    os.system(f"ydotool type '{content}'")
-
-if __name__ == "__main__":
-    type_content("Hello world to the better future")
